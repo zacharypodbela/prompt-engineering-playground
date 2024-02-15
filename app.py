83f558ca45dfd5ac4455fd03da0ccbe32be7dafa
Zachary Podbela <skierzp@me.com>
2024-02-14 22:37:28 -0500

    Set Open AI model to be 3.5 turbo

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def init_chain(llm):
 
 @st.cache_data
 def query_openai(system, prompt):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0125')
     chain = init_chain(llm)
     return chain.invoke({ "system": system, "prompt": prompt })
 
