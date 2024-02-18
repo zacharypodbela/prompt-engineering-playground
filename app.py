ac23e86aa2cd468e18598f25026977352f11ece6
Zachary Podbela <skierzp@me.com>
2024-02-18 13:57:41 -0500

    First Draft of Prompt Chaining

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from string import Formatter
 
 import streamlit as st
+from pydash import partition
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -10,16 +11,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-## OpenAI Initialization
-
-# Set the OpenAI model here with model_name=
-# "text-davinci-003" (the default) is the most powerful but costs $0.02/1000 (very expensive)
-# In order of decreasing power and cost the other options are: 
-# "text-curie-001" (1/10 the cost of davinci)
-# "text-babbage-001" (1/40 the cost of davinci)
-# "text-ada-001" (1/50 the cost of davinci)
-# You can also use non-OpenAI models by instantiating a different LLM class here.
-# See: https://python.langchain.com/en/latest/modules/models/llms/integrations.html
+## Model Functions
 
 def init_chain(llm):
     prompt = ChatPromptTemplate.from_messages([
@@ -42,57 +34,117 @@ def query_ollama(system, prompt):
     chain = init_chain(llm)
     return chain.invoke({ "system": system, "prompt": prompt })
 
-st.header("Model Input")
-system, prompt = None, None # system and prompt will be the outputs of this section
-prompt_buidler_modes = ["Plain Text Prompt Entry", "Templates and Variable Prompt Builder"]
-prompt_builder_mode = st.radio("Choose whether to enter exact system and user prompts or build prompts using templates and data:", prompt_buidler_modes)
-prompt_builder_i = prompt_buidler_modes.index(prompt_builder_mode)
-if prompt_builder_i == 0:
-    # Prompt Entry Fields
-    system = st.text_area('Enter the system instructions here:', value="Always speak in a friendly way.")
-    prompt = st.text_area('Enter your user prompt here:', value="Can you tell me about science?")
-elif prompt_builder_i == 1:
-    # Template Entry
-    _system = st.text_area('Enter the system instructions *template* here:', value="Always speak in a {tone} way.")
-    _prompt = st.text_area('Enter your user prompt here:', value="Can you tell me about {topic}?")
-    template_keys = [i[1] for i in Formatter().parse(_system+" "+_prompt) if i[1] is not None]
+## State Management
 
-    # Data Editor
-    st.write("Add your data to be inserted in the templates above here:")
-    edited_data = st.data_editor(
-        {
-            "tone": "friendly",
-            "topic": "science"
-        }, 
-        use_container_width=True, 
-        num_rows="dynamic", 
-        column_config={ "_index": "Variable Name", "value": "Value" }
-    )
-    missing_keys = [key for key in template_keys if key not in edited_data]
+if 'number_prompts' not in st.session_state:
+    st.session_state.number_prompts = 1
+    
+def increase_prompt_count():
+    st.session_state.number_prompts = st.session_state.number_prompts + 1
 
-    # Prompt Preview
-    with st.container(border=True):
-        st.subheader("Compiled Prompt")
-        if missing_keys:
-            st.markdown(f"⛔️ Set the following missing variables to generate prompts: `{'`, `'.join(missing_keys)}`")
+def decrease_prompt_count():
+    st.session_state.number_prompts = st.session_state.number_prompts - 1
+
+if 'prompt_responses' not in st.session_state:
+    st.session_state.prompt_responses = {}
+
+def save_prompt_response(prompt_index, response):
+    st.session_state.prompt_responses[prompt_index] = response
+
+def return_responses_of_previous_prompts_as_template_data(prompt_index):
+    return { f"prompt_{i}": st.session_state.prompt_responses[i] for i in range(1, prompt_index) }
+
+## Streamlit UI
+
+def render_prompt(index):
+    multiple_prompt_mode = st.session_state.number_prompts > 1
+    is_first_prompt = index == 1
+    is_last_prompt = index == st.session_state.number_prompts
+
+    if st.session_state.number_prompts > 1:
+        st.title(f"Prompt {index}")
+        if is_last_prompt:
+            st.button("Delete this prompt", on_click=decrease_prompt_count)
+    st.header("Model Input")
+    system, prompt = None, None # system and prompt will be the outputs of this section
+    prompt_buidler_modes = ["Plain Text Prompt Entry", "Templates and Variable Prompt Builder"]
+    prompt_builder_mode = st.radio("Choose whether to enter exact system and user prompts or build prompts using templates and data:", prompt_buidler_modes, index=1 if multiple_prompt_mode and not is_first_prompt else 0, key=f"Prompt Builder Mode {index}")
+    prompt_builder_i = prompt_buidler_modes.index(prompt_builder_mode)
+    _system = st.text_area(f"Enter the system instructions here:", key=f"System {index}")
+    _prompt = st.text_area(f"Enter your user prompt here:", key=f"Prompt {index}")
+    if prompt_builder_i == 0:
+        # Prompt Entry Fields
+        system = _system
+        prompt = _prompt
+    elif prompt_builder_i == 1:
+        # Template Entry
+        template_keys = [i[1] for i in Formatter().parse(_system+" "+_prompt) if i[1] is not None]
+
+        # Data Editor
+        st.write("Add your data to be inserted in the templates:")
+        data_frame_element_config = {
+            "use_container_width": True, 
+            "column_order": ["_index", "value"],
+            "column_config": { "_index": "Variable Name", "value": "Value" },
+        }
+        edited_data = st.data_editor(
+            {
+                "tone": "friendly"
+            },
+            key=f"Data Editor {index}",
+            num_rows="dynamic",
+            **data_frame_element_config
+        )
+        data_from_previous_prompts = return_responses_of_previous_prompts_as_template_data(index)
+        if data_from_previous_prompts:
+            st.write("Plus you can leverage the output of previous prompts as variables in the templates:")
+            st.dataframe(return_responses_of_previous_prompts_as_template_data(index), **data_frame_element_config)
+
+        all_data = {**data_from_previous_prompts, **edited_data}
+        missing_keys = [key for key in template_keys if key not in all_data]
+
+        # Prompt Preview
+        with st.container(border=True):
+            st.subheader("Compiled Prompt")
+            if missing_keys:
+                [missing_prompt_keys, missing_normal_keys] = partition(missing_keys, lambda k: k.startswith("prompt_"))
+                if missing_normal_keys:
+                    st.markdown(f"⛔️ Set the following missing variables to generate prompts: `{'`, `'.join(missing_normal_keys)}`")
+                if missing_prompt_keys:
+                    st.markdown(f"⛔️ Ensure all previous prompts have run to generate values for: `{'`, `'.join(missing_prompt_keys)}`")
+            if not _system:
+                st.write("⛔️ Enter a system prompt template.")
+            if not _prompt:
+                st.write("⛔️ Enter a user prompt template.")
+            
+            if not missing_keys and _system and _prompt:
+                system = _system.format(**all_data)
+                prompt = _prompt.format(**all_data)
+                st.write(f"System instructions that will be passed to model:")
+                st.markdown(f"```\n{system}\n```")
+                st.write(f"User prompt that will be passed to model:")
+                st.markdown(f"```\n{prompt}\n```")
+
+    st.header("Model Settings")
+    llm_choice = st.radio("Choose the language model", ("Ollama (Free)", "OpenAI (Paid)"), key=f"Model Choice {index}")
+    run_on_change = st.checkbox("Automatically re-run the model on change of input?", value=llm_choice == "Ollama (Free)", key=f"Run on Change {index}")
+
+    st.header("Model Response")
+    should_run = True if run_on_change else st.button("Run Model", key=f"Run Model {index}")
+    result = None
+    if system and prompt and llm_choice and should_run:
+        result = query_ollama(system, prompt) if llm_choice == "Ollama (Free)" else query_openai(system, prompt)
+        st.write(result)
+        if is_last_prompt:
+            st.button("Chain the output of this prompt to another prompt", on_click=increase_prompt_count)
         else:
-            system = _system.format(**edited_data)
-            prompt = _prompt.format(**edited_data)
-            st.write(f"System instructions that will be passed to model:")
-            st.markdown(f"```{system}```")
-            st.write(f"User prompt that will be passed to model:")
-            st.markdown(f"```{prompt}```")
+            with st.container(border=True):
+                st.caption(f"Use the output of this prompt in subsequent prompts by referencing {{prompt_{index}}} in the system or user prompt templates.")
+    elif not system or not prompt:
+        st.write("⛔️ Finish entering or building your input prompts to generate an output.")
+    elif not llm_choice:
+        st.write("⛔️ Finish selecting configurations for the model in the Model Settings section to generate an output.")
+    save_prompt_response(index, result)
 
-st.header("Model Settings")
-llm_choice = st.radio("Choose the language model", ("Ollama (Free)", "OpenAI (Paid)"))
-run_on_change = st.checkbox("Automatically re-run the model on change of input?", value=True)
-
-st.header("Model Response")
-should_run = True if run_on_change else st.button("Run Model")
-if system and prompt and llm_choice and should_run:
-    result = query_ollama(system, prompt) if llm_choice == "Ollama (Free)" else query_openai(system, prompt)
-    st.write(result)
-elif not system or not prompt:
-    st.write("⛔️ Finish entering or building your input prompts to generate an output.")
-elif not llm_choice:
-    st.write("⛔️ Finish selecting configurations for the model in the Model Settings section to generate an output.")
+for i in range(st.session_state.number_prompts):
+    render_prompt(i+1)
